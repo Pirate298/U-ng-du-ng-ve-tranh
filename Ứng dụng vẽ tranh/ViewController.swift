d2a6eb6a4c0759f0cf9fb2a3fe853088a1e6c1e5
Pirate298 <pirate@PIRATEs-MacBook-Pro.local>
2016-10-25 15:57:19 +0700

    Initial Commit

---
+++ Ứng dụng vẽ tranh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ứng dụng vẽ tranh
+//
+//  Created by PIRATE on 10/25/16.
+//  Copyright © 2016 PIRATE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
